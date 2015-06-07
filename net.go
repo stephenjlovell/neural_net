34abdee79263ae855236dab7f312c9da1d953974
Steve Lovell <sjlovell34@gmail.com>
2015-06-07 11:47:03 -0400

    initial commit

---
+++ net.go
@@ -0,0 +1,22 @@
+
+type Net struct {
+
+}
+
+func (net *Net) FeedForward() {
+
+}
+
+func (net *Net) BackPropegate() {
+  
+}
+
+func (net *Net) CalculateResults() {
+  
+}
+
+func NewNet() *Net {
+  return &Net{
+    
+  }
+}
